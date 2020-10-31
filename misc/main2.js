aa0db4b2810411ea1adfa884235532f444c359ef
Vidu <viduwebjob@gmail.com>
2020-10-31 22:13:11 +0300

    upload project

---
+++ misc/main2.js
@@ -0,0 +1,18 @@
+var app2 = new Vue({
+    el: 'hello',
+    data: {
+        hello: 'hello!'
+        // forMeterSquare: 'unit--active',
+        // forPack: ''
+    },
+    methods: {
+        // priceForPack (){
+        //     this.forPack = 'unit--active';
+        //     this.forMeterSquare = '';
+        // },
+        // priceForMeterSquare (){
+        //     this.forPack = '';
+        //     this.forMeterSquare = 'unit--active';
+        // }
+    }
+});
